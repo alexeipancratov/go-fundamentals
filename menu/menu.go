8414221541440480248a1a73aaedefaf509f9afe
Alexei Pancratov <drivergear@gmail.com>
2023-04-30 11:51:58 -0400

    Initial commit

---
+++ menu/menu.go
@@ -0,0 +1,50 @@
+package menu
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"os"
+	"strings"
+)
+
+type menuItem struct {
+	name   string
+	prices map[string]float64
+}
+
+type menu []menuItem
+
+func (m menu) print() {
+	for _, item := range m {
+		fmt.Println(item.name)
+		fmt.Println(strings.Repeat("-", 10))
+		for size, cost := range item.prices {
+			fmt.Printf("\t%10s%10.2f\n", size, cost)
+		}
+	}
+}
+
+func (m *menu) add() error {
+	fmt.Println("Please enter the name of the new item")
+	name, _ := in.ReadString('\n')
+	name = strings.TrimSpace(name)
+
+	for _, item := range data {
+		if item.name == name {
+			return errors.New("menu item already exists")
+		}
+	}
+	data = append(data, menuItem{name: name, prices: make(map[string]float64)})
+	return nil
+}
+
+var in = bufio.NewReader(os.Stdin)
+
+func Print() {
+	data.print()
+}
+
+func Add() error {
+	return data.add()
+}
